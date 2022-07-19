f66ef8d972e37eb746e991a94a0ac66babacfd09
tasmankingsley <tasman.kingsley@gmail.com>
2022-07-19 22:33:46 +0900

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script>
+  import Tabs from "./lib/tabs.svelte";
+  import Feed from "./lib/feed.svelte";
+  import Menu from "./lib/menu.svelte";
 
 </script>
 
 <main>
-
+  <Tabs/>
 
 </main>
 
 
 <style>
-  
+
 </style>
